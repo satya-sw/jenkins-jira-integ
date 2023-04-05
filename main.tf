b19e6bc94375be38706873108c6f0046a23ad787
satya-sw <127938860+satya-sw@users.noreply.github.com>
2023-04-05 15:26:19 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.21.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  vpc_id = var.vpc_id
+  cidr_block = var.cidr_block
+  private_subnets = var.private_subnets
+  public_subnets = var.public_subnets
+}
+
+module "iam" {
+  source = "./modules/iam"
+  role_name = var.role_name
+  s3_bucket_arn = var.s3_bucket_arn
+}
+
+module "eks" {
+  source = "./modules/eks"
+  cluster_name = var.cluster_name
+  vpc_id = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  public_subnets = module.vpc.public_subnets
+  iam_role_arn = module.iam.iam_role_arn
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "iam_role_arn" {
+  value = module.iam.iam_role_arn
+}
